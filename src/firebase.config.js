5b7cd37fa1acfa69b11418b6ee900e1943d0be2f
ariyansuhan009 <ariyansuhan009@gmail.com>
2021-10-07 21:03:50 +0600

    allmost done

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDBmrq5CzouF8AD61KfQ0aMIyJFfTSUWvg",
+  authDomain: "fire-auth2-93e46.firebaseapp.com",
+  projectId: "fire-auth2-93e46",
+  storageBucket: "fire-auth2-93e46.appspot.com",
+  messagingSenderId: "940318620535",
+  appId: "1:940318620535:web:461194ab29f8565e97ff72"
+  };
+
+  export default firebaseConfig;
